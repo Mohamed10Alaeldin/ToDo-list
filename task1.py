c23915f081abd0f7bb949af892be67cba6f01615
mohamed <mohammed204alaa@gmail.com>
2023-12-15 17:33:24 +0200

    Update task1.py
    to do list V0.1

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 # window is the perant window
 window = tk.Tk()
 # size and (x, y) points
@@ -25,28 +26,22 @@ lb = tk.Listbox(
     
 )
 lb.pack(side=tk.LEFT, fill=tk.BOTH)
-
-task_list = ['Eat apple',
-    'drink water',
-    'go gym',
-    'write software',
-    'write documentation about project',
-    'take a nap',
-    'Learn something',
-    'paint canvas',
-    'listen to recitition',
-    "read for exams",
-    "revise math rulus",
-    "revise math rulus",
-    "revise math rulus",
-    "revise math rulus",
-    "revise math rulus",
-    "revise math rulus"
-    ]
+# task_list = ['Eat apple',
+#     'drink water',
+#     'go gym',
+#     'write software',
+#     'write documentation about project',
+#     'take a nap',
+#     'Learn something',
+#     'paint canvas',
+#     'listen to recitition',
+#     "read for exams",
+#     "revise math rulus"
+#     ]
 
 
-for item in task_list:
-    lb.insert(tk.END, item) # end means add the item to (end of the list)
+# for item in task_list:
+#     lb.insert(tk.END, item) # end means add the item to (end of the list)
 sb = tk.Scrollbar(frame)
 sb.pack(side=tk.RIGHT, fill=tk.BOTH)
 lb.config(yscrollcommand=sb.set)
@@ -56,6 +51,43 @@ my_entry = tk.Entry(
     window,
     font=('times', 24)
     )
-
 my_entry.pack(pady=20)
+button_frame = tk.Frame(window)
+button_frame.pack(pady=20)
+
+def newTask():
+    task = my_entry.get()
+    if task != "":
+        lb.insert(tk.END, task)
+        my_entry.delete(0, "end")
+    else:
+        messagebox.showwarning("warning", "Please enter some task.")
+
+addTask_btn = tk.Button(
+    button_frame,
+    text='Add Task',
+    font=('times 14'),
+    bg='#c5f776',
+    padx=20,
+    pady=10,
+    command= newTask
+)
+addTask_btn.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
+
+def deleteTask():
+    if tk.ANCHOR:
+        lb.delete(tk.ANCHOR)
+    else:
+        messagebox.showwarning("warning", "Please select some task.")
+
+delTask_btn = tk.Button(
+    button_frame,
+    text='Delete Task',
+    font=('times 14'),
+    bg='#ff8b61',
+    padx=20,
+    pady=10,
+    command= deleteTask
+)
+delTask_btn.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
 window.mainloop()
